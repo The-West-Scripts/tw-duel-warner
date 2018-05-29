3c6159c7a18d1d43adb306aff5bde3ca1f77807c
Jan <Jan.Frei@eps.ch>
2018-05-29 11:22:27 +0200

    build updated (so it adds the UserScript header by its own)

---
+++ src/Script-Header.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        The West Duel Warner
+// @author      Mr. Perseus
+// @namespace   tw-perseus
+// @description Warns if somebody is in duel range.
+// @include     https://*.the-west.*/game.php*
+// @include     http://*.the-west.*/game.php*
+// @include     https://*.tw.innogames.*/game.php*
+// @include     http://*.tw.innogames.*/game.php*
+// @version     0.2.1
+// @grant       none
+// ==/UserScript==
+
